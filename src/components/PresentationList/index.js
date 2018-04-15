ec39b83045654501e93052d3fc7592ff90f4bdb0
Alex Holachek <aholachek@gmail.com>
2018-04-15 12:07:43 -0400

    minor fixes

--- src/components/PresentationList/index.js
+++ src/components/PresentationList/index.js
@@ -1,44 +1,32 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import anime from "animejs"
-import Card from "./Card"
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import anime from 'animejs';
+import Card from './Card';
 
 class PresentationList extends Component {
   static propTypes = {
     title: PropTypes.string,
     links: PropTypes.array,
     animatingOut: PropTypes.bool
-  }
+  };
 
-  state = {}
+  state = {};
 
   componentDidMount() {
-    if (this.props.animatingOut) return
-    const content = this.el.querySelector(".page--list__content")
-    anime
-      .timeline()
-      // .add({
-      //   delay: 500,
-      //   begin: () => {
-      //     content.style.display = "block"
-      //   },
-      //   targets: content,
-      //   translateY: ["50vh", 0],
-      //   easing: "easeInSine",
-      //   duration: 300
-      // })
-      .add({
-        targets: content.querySelectorAll(".card"),
-        opacity: [0, 1],
-        delay: (x, i) => i * 200 + 500,
-        duration: 250,
-        easing: "easeInSine",
-        offset: "-=100"
-      })
+    if (this.props.animatingOut) return;
+    const content = this.el.querySelector('.page--list__content');
+    anime({
+      targets: content.querySelectorAll('.card'),
+      opacity: 1,
+      translateY: [-30, 0],
+      delay: (x, i) => i * 100 + 300,
+      duration: 1000,
+      elasticity: 250
+    });
   }
 
   render() {
-    const { links, title, simple } = this.props
+    const { links, title, simple } = this.props;
     return (
       <div className="page--list" ref={el => (this.el = el)}>
         <div>
@@ -47,19 +35,20 @@ class PresentationList extends Component {
           </div>
           <div className="page--list__content">
             <div>
-              <ul className={`card-grid ${simple ? 'card-grid--simple' : ''}`}
+              <ul
+                className={`card-grid ${simple ? 'card-grid--simple' : ''}`}
                 ref={el => {
-                  this.list = el
+                  this.list = el;
                 }}
               >
-                {links.map(l => <Card {...l}  simple={simple}/>)}
+                {links.map(l => <Card {...l} simple={simple} />)}
               </ul>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default PresentationList
+export default PresentationList;
