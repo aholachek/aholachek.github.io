d18f3d4a1c541d35941e76c3f1f6b2bf50e87ea3
aholachek <alex.holachek@datadoghq.com>
2020-10-27 20:42:42 -0400

    Updates

--- component---src-pages-404-js-9ea03048c5ce6fd087f9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{177:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-9ea03048c5ce6fd087f9.js.map
